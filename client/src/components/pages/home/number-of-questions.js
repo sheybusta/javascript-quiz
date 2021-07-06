6a30108c879769c211abacd183c329d32622d97e
IrinaSing <irinasingh.mail@yandex.ru>
2021-07-06 17:03:37 +0200

    new div for num of qu

--- client/src/components/pages/home/number-of-questions.js
+++ client/src/components/pages/home/number-of-questions.js
@@ -1,12 +1,8 @@
 export const numberOfQu = () => {
   const divInst = document.createElement("div");
   divInst.classList = "quiz-instructions";
-  const titleInst = document.createElement("h2");
-  titleInst.innerText = "You have ${} questions to answer";
-  const pInst = document.createElement("p");
-  pInst.innerText =
-    " The test is not official, it's just a nice way to see how much you know, or don't know, about JavaScript. You will get 1 point for each correct answer. At the end of the Quiz, your total score will be displayed. Maximum score is 10 points.";
-  divInst.appendChild(titleInst);
-  divInst.appendChild(titleInst.appendChild(pInst));
+  const titleQu = document.createElement("h2");
+  titleQu.innerText = "You have ${} questions to answer";
+  divInst.appendChild(titleQu);
   return divInst;
 };
