cccea11b8205249d39ce848fa0e243e4319b0d92
IrinaSing <irinasingh.mail@yandex.ru>
2021-07-06 18:09:32 +0200

    divQu

--- client/src/components/pages/home/number-of-questions.js
+++ client/src/components/pages/home/number-of-questions.js
@@ -1,11 +1,11 @@
 import { state } from "../../../init/state.js";
 
 export const numberOfQu = () => {
-  const divInst = document.createElement("div");
-  divInst.classList = "numberofQuDiv";
+  const divQu = document.createElement("div");
+  divQu.classList = "numberofQuDiv";
   const titleQu = document.createElement("h2");
   titleQu.innerText = `
   You have ${state.questions.length} questions to answer.`;
-  divInst.appendChild(titleQu);
-  return divInst;
+  divQu.appendChild(titleQu);
+  return divQu;
 };
