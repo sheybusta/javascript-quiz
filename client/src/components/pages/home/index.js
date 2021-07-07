29b2a92b90facd740d1359c980a961cff6309aad
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-07 10:58:32 +0200

    commented code is deleteds

--- client/src/components/pages/home/index.js
+++ client/src/components/pages/home/index.js
@@ -13,11 +13,7 @@ export const home = () => {
   container.className = 'body';
 
   container.appendChild(instructions());
-  // container.appendChild(
-  //   instructions(
-  //     "You can test your JavaScript skills with Us! The test is not official, it's just a nice way to see how much you know, or don't know, about JavaScript. You will get 1 point for each correct answer. At the end of the Quiz, your total score will be displayed. Maximum score is 10 points."
-  //   )
-  // );
+
   container.appendChild(numberOfQu());
 
   container.appendChild(startQuizButton());
