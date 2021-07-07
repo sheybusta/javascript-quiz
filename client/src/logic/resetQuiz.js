e0238c5d407d4c86b086f16503eca31777b7a0ba
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-07-07 07:43:11 +0200

    reset quiz JS doc and answers reset logic

--- client/src/logic/resetQuiz.js
+++ client/src/logic/resetQuiz.js
@@ -1,9 +1,32 @@
 import { state } from '../init/state.js';
 
+/**
+ * This function reset the state of quiz:
+ * - resets indexOfRenderedQuestion,
+ * - resets currentScore, and
+ * - resets the state of each question
+ */
 export const resetQuiz = () => {
   state.indexOfRenderedQuestion = 0;
   state.currentScore = 0;
+
+  resetQuestions();
   //   debugger;
   //   console.log(state.indexOfRenderedQuestion);
   //   console.log(state.currentScore);
+};
+
+/**
+ * This function resets the state of each question in the quiz:
+ * - resets answered property and
+ * - resets selected property of each available answer for each question
+ */
+const resetQuestions = () => {
+  state.questions.forEach((question) => {
+    question.answered = false;
+
+    question.answers.forEach((answer) => {
+      selected = false;
+    });
+  });
 };
