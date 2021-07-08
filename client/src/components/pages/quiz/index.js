da03e58a1298939456a70cdde556083d5c0589c6
Sheyla <shel.busta@gmail.com>
2021-07-08 13:33:57 +0200

    quiz question render

--- client/src/components/pages/quiz/index.js
+++ client/src/components/pages/quiz/index.js
@@ -1,5 +1,6 @@
-import { state } from '../../../init/state.js';
-import { quizQuestionPanel } from './quiz-question-panel.js';
+// import { state } from "../../../init/state.js";
+import { quizQuestionPanel } from "./quiz-question-panel.js";
+import { createRestartQuizButtonPanel } from "./restart-button.js";
 
 /**
  * The quiz page. Loads when the quiz page is requested.
@@ -7,20 +8,21 @@ import { quizQuestionPanel } from './quiz-question-panel.js';
  * @returns {HTMLDivElement} A rendered quiz page.
  */
 export const quiz = () => {
-  const container = document.createElement('div');
-  container.className = 'body';
+  const container = document.createElement("div");
+  container.className = "body";
 
-  if (state.indexOfRenderedQuestion >= 0) {
-    //Main display panel for the questions
-    const mainQuestionPanel = quizQuestionPanel();
+  // if (state.indexOfRenderedQuestion >= 0) {
+  //   //Main display panel for the questions
+  //   const mainQuestionPanel = quizQuestionPanel();
 
-    container.appendChild(mainQuestionPanel);
-  } else {
-    //warn if the start button is never clicked
-    container.appendChild(
-      document.createTextNode('Please start the quiz from home page!')
-    );
-  }
-
+  //   container.appendChild(mainQuestionPanel);
+  // } else {
+  //   //warn if the start button is never clicked
+  //   container.appendChild(
+  //     document.createTextNode('Please start the quiz from home page!')
+  //   );
+  // }
+  container.appendChild(createRestartQuizButtonPanel());
+  container.appendChild(quizQuestionPanel());
   return container;
 };
