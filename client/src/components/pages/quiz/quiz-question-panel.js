72e39775425e47c5135e06e195f6be07417f306e
Sheyla <shel.busta@gmail.com>
2021-07-09 15:15:34 +0200

    checked function

--- client/src/components/pages/quiz/quiz-question-panel.js
+++ client/src/components/pages/quiz/quiz-question-panel.js
@@ -4,6 +4,27 @@ import { state } from "../../../init/state.js";
 // import { createQuizButtonsDiv } from "./quiz-buttons.js";
 // import { createRestartQuizButtonPanel } from "./restart-button.js";
 
+const checked = (e) => {
+  debugger;
+  const target = e.target;
+  if (target.nodeName === "INPUT") {
+    const isChecked = target.checked;
+
+    if (isChecked) {
+      state.questions[state.indexOfRenderedQuestion].answers[
+        e.target.dataset.index
+      ].selected = true;
+    } else {
+      state.questions[state.indexOfRenderedQuestion].answers[
+        e.target.dataset.index
+      ].selected = false;
+    }
+    console.log(isChecked);
+    console.log(target);
+    console.log(e.target.dataset.index);
+  }
+};
+
 export const quizQuestionPanel = (indexQuestion) => {
   const quizContainer = document.createElement("div");
   quizContainer.className = "quiz";
