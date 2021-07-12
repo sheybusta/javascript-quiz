6c087aa69c205e64dd72823bbb2baf93cffc1efc
ERKAM GURESEN <erkamguresen@gmail.com>
2021-07-12 01:04:36 -0700

    linting corrections

--- client/src/components/pages/quiz/hint-div.js
+++ client/src/components/pages/quiz/hint-div.js
@@ -1,27 +1,26 @@
-import { button } from '../../shared/button.js';
-import { divElement } from '../../shared/div.js';
+import { button } from "../../shared/button.js";
+import { divElement } from "../../shared/div.js";
 
 export const createHint = (hint, index) => {
-  const hintDiv = divElement('btn-group mt-5 mb-2', 'hint-panel');
+  const hintDiv = divElement("btn-group mt-5 mb-2", "hint-panel");
 
-  // create the dropdown buttons
-  const firstButton = button('button', `Hint${index + 1}`, 'btn btn-info ');
+  const firstButton = button("button", `Hint${index + 1}`, "btn btn-info ");
 
   const dropdownButton = button(
-    'button',
+    "button",
     ``,
-    'btn btn-info dropdown-toggle dropdown-split mr-2'
+    "btn btn-info dropdown-toggle dropdown-split mr-2"
   );
 
-  dropdownButton.dataset.toggle = 'dropdown';
+  dropdownButton.dataset.toggle = "dropdown";
 
-  const dropdownMenu = divElement('dropdown-menu');
+  const dropdownMenu = divElement("dropdown-menu");
 
-  const aElement = document.createElement('a');
-  aElement.className = 'dropdown-item';
+  const aElement = document.createElement("a");
+  aElement.className = "dropdown-item";
   aElement.innerText = hint.text;
   aElement.href = hint.url;
-  aElement.target = '_blank';
+  aElement.target = "_blank";
 
   dropdownMenu.appendChild(aElement);
 
--- client/src/components/pages/quiz/index.js
+++ client/src/components/pages/quiz/index.js
@@ -1,8 +1,7 @@
-import { state } from '../../../init/state.js';
-import { divElement } from '../../shared/div.js';
-import { createHintPanel } from './hint-panel.js';
-import { quizQuestionPanel } from './quiz-question-panel.js';
-import { createRestartQuizButtonPanel } from './restart-button.js';
+import { state } from "../../../init/state.js";
+import { createHintPanel } from "./hint-panel.js";
+import { quizQuestionPanel } from "./quiz-question-panel.js";
+import { createRestartQuizButtonPanel } from "./restart-button.js";
 
 /**
  * The quiz page. Loads when the quiz page is requested.
@@ -10,8 +9,8 @@ import { createRestartQuizButtonPanel } from './restart-button.js';
  * @returns {HTMLDivElement} A rendered quiz page.
  */
 export const quiz = () => {
-  const container = document.createElement('div');
-  container.className = 'body';
+  const container = document.createElement("div");
+  container.className = "body";
 
   if (state.indexOfRenderedQuestion >= 0) {
     container.appendChild(quizQuestionPanel(state.indexOfRenderedQuestion));
@@ -21,7 +20,7 @@ export const quiz = () => {
   } else {
     //warn if the start button is never clicked
     container.appendChild(
-      document.createTextNode('Please start the quiz from home page!')
+      document.createTextNode("Please start the quiz from home page!")
     );
   }
 
