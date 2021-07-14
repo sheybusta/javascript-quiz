bde816450eca0e5cff7a16283d30246836b7b91f
fatih <fatihayoz20800@gmail.com>
2021-07-14 15:59:35 +0200

    next button renders the next question

--- client/src/handlers/nextButtonHandler.js
+++ client/src/handlers/nextButtonHandler.js
@@ -1,16 +1,19 @@
 import { data } from "../../data/quiz.js";
 import { quizQuestionPanel } from "../components/pages/quiz/quiz-question-panel.js";
 
-let numberOfTotalQuestions = 0;
+let numberOfTotalQuestions = 1;
 
 const nextButtonHandler = () => {
   if (numberOfTotalQuestions < data.questions.length) {
+    // empty the container
+    document.querySelector(".quiz").innerHTML = "";
+    const quizContainer = document.querySelector(".quiz");
+    // render the question panel
+    quizContainer.appendChild(quizQuestionPanel(numberOfTotalQuestions));
     numberOfTotalQuestions++;
-    quizQuestionPanel(numberOfTotalQuestions);
   } else {
     console.log("alert/error");
   }
-  return nextButtonHandler;
 };
 
 export { nextButtonHandler };
