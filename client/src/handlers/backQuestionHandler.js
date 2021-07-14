a8c63daf5c963fa8cf1da917a36354527364d004
Sheyla <shel.busta@gmail.com>
2021-07-14 14:15:44 +0200

    handler of back question

--- client/src/handlers/backQuestionHandler.js
+++ client/src/handlers/backQuestionHandler.js
@@ -1,5 +1,23 @@
+import { state } from "../init/state";
+
 const backQuestionHandler = () => {
   console.log("here");
+  // clean quiz
+  const quiz = document.getElementsByClassName("quiz");
+  quiz.innerHTML = "";
+
+  // update the state
+  state.indexOfRenderedQuestion -= 1;
+  console.log(state.indexOfRenderedQuestion);
+
+  // get length of questions or total questions
+  const totalQuestions = state.numberOfTotalQuestions;
+  // condition if index of question is less than total questions
+  if (state.indexOfRenderedQuestion >= totalQuestions) {
+    state.indexOfRenderedQuestion = state.numberOfTotalQuestions + 1;
+  }
+  console.log(state.indexOfRenderedQuestion);
+  console.log(state.numberOfTotalQuestions);
 };
 
 export { backQuestionHandler };
