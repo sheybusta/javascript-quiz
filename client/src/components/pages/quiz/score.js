766fca3ba0d9bdd3e11d23ff7b6ac5cca5c39d9c
IrinaSing <irinasingh.mail@yandex.ru>
2021-07-16 11:59:19 +0200

    description of the component

--- client/src/components/pages/quiz/score.js
+++ client/src/components/pages/quiz/score.js
@@ -1,3 +1,10 @@
+/**
+ * Returns a div element.
+ *
+ * @param {number} updatedScore - Updated score from data.
+ * @returns {HTMLDivElement} - Div with rendered score.
+ */
+
 export const score = (updatedScore) => {
   const div = document.createElement("div");
   div.id = "div-score";
